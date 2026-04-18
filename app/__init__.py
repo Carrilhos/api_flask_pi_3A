7643eb42361dcb3a971ebd927b52ac6f74840a20
Gabriel Carrilhos <gcarrilhos@gmail.com>
2026-04-17 21:24:17 -0300

    adicionado deploy

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,18 @@
 from flask import Flask
 from app.config import Config
+
+#Pai nosso que estais nos Céus,
+#santificado seja o vosso Nome,
+#venha a nós o vosso Reino,
+#seja feita a vossa vontade assim na terra como no Céu.
+#O pão nosso de cada dia nos dai hoje,
+#perdoai-nos as nossas ofensas
+#assim como nós perdoamos a quem nos tem ofendido,
+#e não nos deixeis cair em tentação,
+#mas livrai-nos do Mal.
+#Amém.
+
+#Garantia que vai funcionar
 print("INIT EXECUTADO")
 def register_routes(app):
     from app.routes.product_routes import produto_bp
--- run.py
+++ run.py
@@ -1,19 +1,3 @@
-#Pai nosso que estais nos Céus,
-#santificado seja o vosso Nome,
-#venha a nós o vosso Reino,
-#seja feita a vossa vontade assim na terra como no Céu.
-#O pão nosso de cada dia nos dai hoje,
-#perdoai-nos as nossas ofensas
-#assim como nós perdoamos a quem nos tem ofendido,
-#e não nos deixeis cair em tentação,
-#mas livrai-nos do Mal.
-#Amém.
-
-#Garantia que vai funcionar
-
 from app import create_app
 
 app = create_app()
-print("RUN.PY EXECUTADO")
-if __name__ == "__main__":
-    app.run(debug=True)
