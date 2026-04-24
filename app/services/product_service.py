0d1eb8c5da9e71ca70ac484aa03070318506a66d
Nicolas Casser <nicolascasser4@gmail.com>
2026-04-23 22:19:57 -0300

    feat: retorna atributos completos no GET /produtos

--- app/services/product_service.py
+++ app/services/product_service.py
@@ -1,27 +1,47 @@
 from app.repositories.produto_repository import (
     find_all_produtos,
     find_produto_by_id,
-    get_category_attributes
+    get_category_attributes,
+    get_atributos_por_produto,
 )
 
 def get_all_produtos():
+    # Busca todos os produtos
     produtos = find_all_produtos()
-    category_attributes = get_category_attributes()
-    print(category_attributes)
     
     for produto in produtos:
         produto["atributos"] = []
-        for cat_attr in category_attributes:
-            if cat_attr["id_categoria"] == produto["id_categoria"]:
-                produto["atributos"].append({cat_attr["nome_atributo"]: "dado temporario"}) # Substitua "valor" pelo valor real do atributo para o produto
-    # print(produtos)
+        
+        rows_atributos = get_atributos_por_produto(produto["id_produto"])
+        
+        for attr in rows_atributos:
+            nome_attr = attr[0]
+            val_str = attr[1]
+            val_num = attr[2]
+            val_bool = attr[3]
+            
+            # Descobre qual valor está preenchido
+            valor_final = val_str if val_str is not None else (val_num if val_num is not None else val_bool)
+            
+            produto["atributos"].append({ nome_attr: valor_final })
+
     return produtos
 
 def get_produto_by_id(id_produto):
-
     produto = find_produto_by_id(id_produto)
 
     if not produto:
         return None
+        
+    produto["atributos"] = []
+    rows_atributos = get_atributos_por_produto(id_produto)
+    
+    for attr in rows_atributos:
+        nome_attr = attr[0]
+        val_str = attr[1]
+        val_num = attr[2]
+        val_bool = attr[3]
+        valor_final = val_str if val_str is not None else (val_num if val_num is not None else val_bool)
+        produto["atributos"].append({ nome_attr: valor_final })
 
     return produto
